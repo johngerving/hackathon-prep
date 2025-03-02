769d16e057873e88c304d0bdd570c02f7106abbc
johngerving <gervingjohn@gmail.com>
2025-03-01 17:22:58 -0800

    Add types from Supabase

--- src/routes/(private)/+page.server.ts
+++ src/routes/(private)/+page.server.ts
@@ -1,7 +1,10 @@
 import type { PageServerLoad } from './$types';
+import type { Note } from '$lib/types/database';
 
 export const load: PageServerLoad = async ({ depends, locals: { supabase } }) => {
 	depends('supabase:db:notes');
-	const { data: notes } = await supabase.from('notes').select('id,note').order('id');
+	const { data: notes } = (await supabase.from('notes').select('id,note').order('id')) as {
+		data: Note[];
+	};
 	return { notes: notes ?? [] };
 };
